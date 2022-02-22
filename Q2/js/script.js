5c705e6d15d89be29b1bc56c2e66550791f978a5
deffyded <kexin.z@outlook.com>
2022-02-21 23:56:48 -0800

    Initial commit

---
+++ Q2/js/script.js
@@ -0,0 +1,33 @@
+var rating;
+// var stars;
+$(function () {
+	rating = 0; // get current user rating of product not implemented
+	$(".star")
+		.click(function () {
+			rating = parseInt($(this).attr("rating"));
+			$(".star").each(function () {
+				if (parseInt($(this).attr("rating")) <= rating) {
+					$(this).addClass("selected");
+					$(this).removeClass("hover");
+				} else {
+					$(this).removeClass("selected");
+				}
+			});
+			$.ajax({ // post rating to web server
+				type: "POST",
+				url: "", // web server url 
+				data: { "UUID": 0, "rating": rating, "userId": 0 }
+			});
+		})
+		.mouseleave(function () {
+			$(".star").removeClass("hover");
+		})
+		.mouseenter(function () {
+			rating = parseInt($(this).attr("rating"));
+			$(".star").each(function () {
+				if (parseInt($(this).attr("rating")) <= rating) {
+					$(this).addClass("hover");
+				}
+			});
+		});
+});
