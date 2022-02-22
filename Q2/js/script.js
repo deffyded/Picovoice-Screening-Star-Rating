7abfb1b29c25d5d87a179313e6c4907d3c6b6ecd
deffyded <kexin.z@outlook.com>
2022-02-22 02:17:08 -0800

    Update script.js

--- Q2/js/script.js
+++ Q2/js/script.js
@@ -10,14 +10,15 @@ $(function () {
 		})
 		.click(function () {
 			rating = parseInt($(this).attr("rating"));
-			$(".star").each(function () {
-				if (parseInt($(this).attr("rating")) <= rating) {
-					$(this).addClass("selected");
-					$(this).removeClass("hover");
-				} else {
-					$(this).removeClass("selected");
-				}
-			});
+			$(".star")
+				.removeClass("hover")
+				.each(function () {
+					if (parseInt($(this).attr("rating")) <= rating) {
+						$(this).addClass("selected");
+					} else {
+						$(this).removeClass("selected");
+					}
+				});
 			$.ajax({ // post rating to web server
 				type: "POST",
 				url: "", // web server url 
